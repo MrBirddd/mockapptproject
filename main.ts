8a696a5038eb45ed9f1f0920f68dcc3cad9fc40d
bird <114713711+MrBirddd@users.noreply.github.com>
2024-01-11 12:24:38 -0600

    sprite added and tilemap start

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    roseleft.setImage(assets.image`roseleft`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    roseleft.setImage(assets.image`roseright`)
+})
+let roseleft: Sprite = null
+roseleft = sprites.create(assets.image`roseleft`, SpriteKind.Player)
+roseleft.setPosition(80, 90)
+tiles.setCurrentTilemap(tilemap`level1`)
